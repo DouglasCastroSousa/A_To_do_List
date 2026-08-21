f7e1d088f911f6e6b49efceab05c76548e34c012
Diego Castro <diegocastro1087@gmail.com>
2026-08-20 21:39:01 -0300

    preparando estrutura Front end

---
+++ Front-End/src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+
+    return (
+        <header>
+            <div>tarefas</div>
+            <div>Change background</div>
+        </header>
+    )
+}
+
+export default Header;
