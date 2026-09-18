dbf75ad7a5f509553bb909fa974ebb18cb568e23
Diego Castro <diegocastro1087@gmail.com>
2026-09-17 23:01:26 -0300

    atualização no front(aprendendo react)

--- Front-End/src/components/header.jsx
+++ Front-End/src/components/header.jsx
@@ -1,11 +1,14 @@
-function Header(){
+import { FaBuffer } from "react-icons/fa";
 
-    return (
-        <header>
-            <div>tarefas</div>
-            <div>Change background</div>
-        </header>
-    )
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h2>
+          <FaBuffer />
+        </h2>
+      </div>
+      <div>Change background</div>
+    </header>
+  );
 }
-
-export default Header;
