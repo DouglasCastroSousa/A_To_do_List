3cf96a8a7561a6ee911a1267eda0f1a9dbc83a02
Diego Castro <diegocastro1087@gmail.com>
2026-09-22 18:38:40 -0300

    mudança de design, 'inserção' de campos provisorios de task, que futuramente serão gerados automaticamente

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header'
+import Header from './components/header/header'
+import TaskList from "./components/tasklist/tasklist"
+
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <TaskList />
+    </div>
+    
   )
 }
 
---
+++ Front-End/src/components/tasklist/taskChange/taskChange.jsx
@@ -0,0 +1,10 @@
+import "./taskChange.css"
+
+export default function TaskChange() {
+  return (
+    <div>
+      <div id="absolute"></div>
+      <div id="absolute2">asawdadsa</div>
+    </div>
+  );
+}
